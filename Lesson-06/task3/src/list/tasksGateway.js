43ecb80591f8855a3a9afcfe6b5bcfc47014384f
Sergey Filatov <3037677@gmail.com>
2021-09-22 23:35:52 +0300

    1

--- Lesson-06/task3/src/list/tasksGateway.js
+++ Lesson-06/task3/src/list/tasksGateway.js
@@ -1,26 +1,26 @@
-const baseUrl = "https://60c88362afc88600179f73f9.mockapi.io/api/v1/form";
+const baseUrl = 'https://6141db7d4d16670017ba29f9.mockapi.io/api/v1/tasks';
 
 export const createTask = (taskData) =>
   fetch(baseUrl, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json;charset=utf-8",
+      'Content-Type': 'application/json;charset=utf-8',
     },
     body: JSON.stringify(taskData),
   });
 
 export const updateTask = (taskId, taskData) =>
   fetch(`${baseUrl}/${taskId}`, {
-    method: "PUT",
+    method: 'PUT',
     headers: {
-      "Content-Type": "application/json;charset=utf-8",
+      'Content-Type': 'application/json;charset=utf-8',
     },
     body: JSON.stringify(taskData),
   });
 
 export const deleteTask = (taskId) =>
   fetch(`${baseUrl}/${taskId}`, {
-    method: "DELETE",
+    method: 'DELETE',
   });
 
 const mapTasks = (tasks) =>
