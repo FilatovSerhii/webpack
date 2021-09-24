0a084a88b8e42d012788dd91a5b4ba8f1fd6e1b1
Sergey Filatov <3037677@gmail.com>
2021-09-24 19:28:53 +0300

    2

--- Lesson-09/task1/.eslintrc.js
+++ Lesson-09/task1/.eslintrc.js
@@ -1,14 +1,10 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: 'eslint:recommended',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
+  extends: 'eslint-config-airbnb-base',
+
   rules: {
     'no-console': 2,
+  },
+  env: {
+    browser: true,
   },
 };
